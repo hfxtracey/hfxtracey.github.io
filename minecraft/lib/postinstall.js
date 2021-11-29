5e774982fd3c9a696b372adfa9741d3126a776bc
Steven Tracey <steven@nevets.tech>
2021-11-29 16:17:35 -0500

    add Consumer project

--- minecraft/lib/postinstall.js
+++
@@ -1,8 +0,0 @@
-const decompress = require('decompress')
-const decompressTarxz = require('decompress-tarxz')
-const path = require('path')
-
-decompress(
-  path.join(__dirname, '../assets/pack.zip'),
-  path.join(__dirname, '../assets/pack')
-)
